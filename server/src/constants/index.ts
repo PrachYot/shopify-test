1bd9b50a7af98a1c8de53c853b92e44456699bdb
Poafs1 <poafs1@hotamil.com>
2022-11-22 16:20:14 +0700

    feat: add server config

---
+++ server/src/constants/index.ts
@@ -0,0 +1,4 @@
+export const CONSTANTS = {
+  globalPrefix: '/api',
+  apiVersion: '/v1',
+};
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigsService } from './configs/configs.service';
+import * as cookieParser from 'cookie-parser';
+import { CONSTANTS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const { server, clientUrl } = app.get(ConfigsService);
+
+  // CORS
+  const corsOption: CorsOptions = {
+    origin: clientUrl,
+    credentials: true,
+  };
+
+  app.enableCors(corsOption);
+  app.use(cookieParser());
+
+  // Set global prefix
+  app.setGlobalPrefix(CONSTANTS.globalPrefix + CONSTANTS.apiVersion);
+
+  await app.listen(server.port);
 }
 bootstrap();
